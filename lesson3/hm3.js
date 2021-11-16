255078a078e3ec39f3cd46eb2b5971a3013872b0
IgorVykhovanets <igorvykhovanets808@gmail.com>
2021-11-16 17:25:38 +0200

    HM3

---
+++ lesson3/hm3.js
@@ -0,0 +1,107 @@
+// Завдання 1
+
+// let a = [1, 2, 3, 4, 5];
+// let b = ['a', 'b', 'c', 'd', 'e'];
+// let c = [[1, 2, 3, 4, 5], ['a', 'b', 'c', 'd', 'e'], [true, false, 1 < 2, 1 === 1, 3 > 0]];
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// Завдання 2
+
+// let a = [];
+// a = [1, 2, 'string', [true, false]];
+// console.log(`${a[0]} ${a[1]} ${a[2]} ${a[3][0]} ${a[3][1]}`)
+
+// Завдання 3
+
+// for (let i = 0; i < 10; i++) {
+//     document.write('<div>Hello okten</div>')
+// }
+
+// let a = ['hello', 'okten'];
+// for (let i = 0; i < 10; i++) {
+//     document.write(`<div>${a[0]} ${a[1]}</div>`)
+// }
+//
+// let a = 0;
+// while (a <= 20) {
+//     document.write(`<h1>Hello Okten ${a}</h1>`)
+//     a++
+// }
+
+// let a = ['hello', 'okten', 'school'];
+// let z = 0;
+// while (z <= 20) {
+//     document.write(`<h1>${a[0]} ${a[1]} ${a[2]} ${z}</h1>`)
+//     z++
+// }
+
+// Завдання 4
+
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let b = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'k', 'l', 'm'];
+// let c = [1 < 2, 2 === 2, 1 > 3, true, false, 10 < 11, 12 === 12, 'string' === 'string', 'z' === 'z', 5 !== 6];
+//
+// for (let z = 0; z < a.length; z++) {
+//     console.log(a[z])
+// }
+
+// for (let y = 0; y < b.length; y++) {
+//     console.log(b[y])
+// }
+
+// for (let g = 0; g < c.length; g++) {
+//     console.log(c[g])
+// }
+
+// let a = [1, 2, 3, 4, 'hello', 'okten', 'school', 2 < 1, 1 === 0, 2 <= 2];
+// for (z = 0; z < a.length; z++) {
+//     if (typeof a[7] === typeof a[z]) {
+//         console.log(a[z])
+//     }
+// }
+
+// for (z = 0; z < a.length; z++) {
+//     if (typeof a[0] === typeof a[z]) {
+//         console.log(a[z])
+//     }
+// }
+
+// for (z = 0; z < a.length; z++) {
+//     if (typeof a[4] === typeof a[z]) {
+//         console.log(a[z])
+//     }
+// }
+
+// Завдання 5
+
+// let a = [];
+// a = [1, 2, 3, true, false, 'string', [11, 222, 33], 'okten','school'];
+// for (z = 0; z < a.length; z++) {
+//     console.log(a[z]);
+// }
+
+// let a = ['hello', 'okten', 'school'];
+// for (let z = 0; z < 10; z++) {
+//     console.log(`${a[0]} ${a[1]} ${a[2]} ${z}`)
+//     document.write(`<div>${a[0]} ${a[1]} ${a[2]} ${z}</div>`)
+// }
+
+// let a = 'day';
+// for (let z = 0; z < 100; z++) {
+//     console.log(`${a} ${z}`)
+//     document.write(`<div>${a} ${z}</div>`)
+// }
+
+// let a = 'number';
+// for (let z = 0; z < 100; z += 2) {
+//     console.log(`${a} ${z}`)
+//     document.write(`<div>${a} ${z}</div>`)
+// }
+
+// let a = 'day';
+// for (let z = 1; z < 100; z += 2 ) {
+//     console.log(`${a} ${z}`)
+//     document.write(`<div>${a} ${z}</div>`)
+// }
