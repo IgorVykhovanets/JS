0adafe43f1c03060b5491045f2a43a57ba963b64
IgorVykhovanets <igorvykhovanets808@gmail.com>
2021-11-16 17:25:46 +0200

    CW3

---
+++ lesson3/cw3.js
@@ -0,0 +1,110 @@
+
+let a = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+
+// Завдання 1
+
+// let z = 0;
+// while (z < a.length) {
+//     console.log(a[z]);
+//     z++
+// }
+
+// Завдання 2
+
+// for (let z = 0; z < a.length; z++) {
+//     console.log(a[z])
+// }
+
+// Завдання 3
+
+// let z = 1;
+// while (z < a.length) {
+//     console.log(a[z]);
+//     z += 2;
+// }
+//
+// for (let y = 1; y < a.length; y += 2) {
+//     console.log(a[y])
+// }
+
+// Завдання 4
+
+// let z = 0;
+// while (z < a.length) {
+//     if (a[z] % 2 === 0) {
+//         console.log(a[z])
+//     }
+//     z++;
+// }
+
+// for (let z = 0; z < a.length; z++) {
+//     if (a[z] % 2 === 0) {
+//         console.log(a[z])
+//     }
+// }
+
+// Завдання 5
+
+// for (let z = 0; z <= a.length; z++) {
+//     if (a[z] % 3 === 0) {
+//         a[z] = 'okten';
+//         console.log(a[z])
+//     }
+// }
+
+// Завдання 6
+//
+// for (let z = a.length - 1; z >= 0; z--) {
+//     console.log(a[z])
+// }
+
+// Завдання 1 ( задом наперед )
+
+// let z = a.length - 1;
+// while (z >= 0) {
+//     console.log(a[z]);
+//     z--;
+// }
+
+// Завдання 2 ( задом наперед )
+
+// for (let z = a.length - 1; z >= 0; z--) {
+//     console.log(a[z])
+// }
+
+// Завдання 3 ( задом наперед )
+//
+// let z = a.length - 1;
+// while (z >= 0) {
+//     console.log(a[z])
+//     z = z - 2
+// }
+
+// for (let z = a.length - 1; z >= 0; z = z - 2) {
+//     console.log(a[z])
+// }
+
+// Завдання 4 ( задом наперед )
+
+// let z = a.length - 1;
+// while (z >= 0) {
+//     if (a[z] % 2 === 0) {
+//         console.log(a[z])
+//     }
+//     z--;
+// }
+//
+// for (z = a.length - 1; z >= 0; z--) {
+//     if (a[z] % 2 === 0) {
+//         console.log(a[z])
+//     }
+// }
+
+// Завдання 5 ( задом наперед )
+
+// for (let z = 0; z <= a.length; z++) {
+//     if (a[z] % 3 === 0) {
+//         a[z] = 'okten';
+//         console.log(a[z]);
+//     }
+// }
