3ab5023dd9bb256ab779f5085307a076434d6bf7
IgorVykhovanets <igorvykhovanets808@gmail.com>
2021-11-18 22:16:00 +0200

    HM4

---
+++ lesson4/hm4.js
@@ -0,0 +1,87 @@
+// Завдання 1
+//
+// function s(a,b) {
+//     return console.log("Площа прямокутника " + a * b);
+// }
+// s(5,5)
+
+// Завдання 2
+
+// function s(r) {
+//     return 3.14 * (r ** 2);
+// }
+// console.log(s(4))
+
+// Завдання 3
+
+// function s(r, h) {
+//     return 2 * 3.14 * r * h;
+// }
+// console.log(s(5,5))
+
+// Завдання 4
+//
+// function arrayList(a) {
+//     for (let z = 0; z < a.length; z++) {
+//         console.log(a[z]);
+//     }
+//     return
+// }
+//
+// console.log(arrayList(a = [1, 2, 3, 4, 5]));
+
+// Завдання 5
+
+// function p(a) {
+//     document.write("<p>" + a + "</p>")
+//     return
+// }
+//
+// p('Hello world');
+
+// Завдання 6
+//
+// function list(text) {
+//     document.write(`<ul>`)
+//     document.write(`<li>${text}</li>`)
+//     document.write(`<li>${text}</li>`)
+//     document.write(`<li>${text}</li>`)
+//     document.write(`</ul>`)
+//     return
+// }
+// list('Hello Okten')
+
+// Завдання 7
+
+// function list(text, numberList) {
+//     document.write(`<ul>`)
+//     for (let z = 0; z < numberList; z++) {
+//         document.write(`<li>${text}</li>`)
+//     }
+//     document.write(`</ul>`)
+//     return
+// }
+// list('hello', 5)
+
+// Завдання 8
+
+// function list(array) {
+//     document.write(`<ul>`)
+//     for (let z = 0; z < array.length; z++) {
+//         document.write(`<li>${array[z]}</li>`)
+//     }
+//     document.write(`</ul>`)
+//     return
+// }
+//
+// list(a = [1, 2, 3, 4, 'Hello', 'Okten', true, false]);
+
+// Завдання 9
+
+// function objectList(arr) {
+//     for (let z of arr) {
+//         document.write(`<div> ${z.id} ${z.name} ${z.age} </div>`)
+//     }
+// }
+//
+// objectList(a = [{name: 'igor', age: 21, id: 1}])
