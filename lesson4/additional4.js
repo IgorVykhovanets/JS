515dfea660d4783275a5c81096e73a5d58839fb0
IgorVykhovanets <igorvykhovanets808@gmail.com>
2021-11-19 13:37:41 +0200

    add4

---
+++ lesson4/additional4.js
@@ -0,0 +1,53 @@
+// // Завдання 1
+// function k(a, b) {
+//     if (a + b === a , b) {
+//         console.log(a + ' ' + b)
+//     } else {
+//         console.log(a)
+//     }
+// }
+// k('hello')
+// k('hello', 'okten');
+
+// Завдання 2
+
+// let arr3 = [];
+//
+// function sumArray(arr1, arr2) {
+//     for (let z = 0; z < arr1.length; z++) {
+//         arr3[z] = arr1[z] + arr2[z];
+//     }
+//     console.log(arr3)
+// }
+//
+// sumArray(a = [1, 2, 3, 4], b = [2, 3, 4, 5]);
+
+// Завдання 3
+//
+// let b = [];
+//
+// function sumArray(arr) {
+//     for (let arrElement of arr) {
+//         for (const arrElementKey in arrElement) {
+//             b += arrElementKey + ' '
+//         }
+//     }
+//     console.log(b)
+// }
+//
+// sumArray(a = [{name: 'Dima', age: 13}, {model: 'Camry'}]);
+
+// Завдання 4
+//
+// let b = [];
+//
+// function keyArray(arr) {
+//     for (const arrElement of arr) {
+//         for (const arrElementKey in arrElement) {
+//             b += arrElement[arrElementKey] + ' '
+//         }
+//     }
+//     console.log(b)
+// }
+//
+// keyArray(a = [{name: 'Dima', age: 13}, {model: 'Camry'}]);
